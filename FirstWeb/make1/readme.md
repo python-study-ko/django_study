9e2b64323a982f526f2e1c2e7d95f11db85f448b
sinsky <wesky93@gmail.com>
2016-07-01 21:22:42 +0900

    6. model.py 질문,답변 모델 정의(테이블 정의)

--- FirstWeb/make1/readme.md
+++ FirstWeb/make1/readme.md
@@ -1,4 +1,5 @@
 # 프로젝트 진행
+질문에 던지고 그에대한 답변을 보내고 확인하는 앱입니다.
 
 ### 1. 프로젝트 시작
     django-admin.py startproject make1
@@ -9,3 +10,4 @@
 ### 4. 관리자계정 생성
     python manage.py createsuperuser
 ### 5. settings.py에 polls앱 추가 및 시간대 변경
+### 6. model.py 질문,답변 모델 정의(테이블 정의)
--- FirstWeb/polls/models.py
+++ FirstWeb/polls/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    # 질문 필드(최대 문자열 길이 200)
+    question_text = models.CharField(max_length=200)
+    # 질문 등록시간 필드
+    pub_date = models.DateTimeField('date published')
+
+    # 테이블명 설
+    def __str__(self): # 파이썬2.7에선 __unicode__
+        return self.question_text
+
+class Choice(models.Model):
+    # 답변,질문 연결 필드
+    question = models.ForeignKey(Question)
+    # 답변 문구 필드(최대 문자열 길이 200)
+    choice_text = models.CharField(max_length=200)
+    # 답변수 계측 필드(기본값 0)
+    votes = models.IntegerField(default=0)
+
+    # 테이블정명 설정
+    def __str__(self): # 파이썬2.7에선 __unicode__
+        return self.choice_text
