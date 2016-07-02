db4237c844c3d4d176fb2f440b1efe00f8ae944e
wesky93 <wesky93@gmail.com>
2016-07-02 18:24:07 +0900

    19. admin 꾸미기(다중 테이블 필드를 동시에 띄우기)

--- FirstWeb/polls/admin.py
+++ FirstWeb/polls/admin.py
@@ -3,11 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from polls.models import Question,Choice
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        ('질문 내용',{'fields':['question_text']}),
+        (None,{'fields':['question_text']}),
         ('질문 날짜',{'fields':['pub_date'],'classes':['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question,QuestionAdmin)
 admin.site.register(Choice)
