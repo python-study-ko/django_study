2fcdaf164946e3955daecb2146342fecf03c0f09
sinsky <wesky93@gmail.com>
2016-07-04 10:12:28 +0900

    6.  books 세부 url패턴 추가

---
+++ SecondWeb/books/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns,url
+from book import views
+
+urlpatterns = patterns('',
+                       # 메인페이지
+                       url(r'^$',views.BooksModelView.as_view(),name='index'),
+                       # 목록페이지
+                       url(r'^book/$',views.BookList.as_view(),name='book_list'),
+                       url(r'author/$',views.AuthorList.as_view(),name='author_list'),
+                       url(r'publisher/$',views.PublisherList.as_view(),name='publisher_list'),
+                       # 세부페이지
+                       url(r'book/(?P<pk>\d+)/$',views.BookDetail.as_view(),name='book_detail'),
+                       url(r'author/(?P<pk>\d+)/$',views.AuthorDetail.as_view(),name='author_detail'),
+                       url(r'publisher/(?P<pk>\d+)/$',views.PublisherDetail.as_view(),name='publisher_detail'),
+                       )
